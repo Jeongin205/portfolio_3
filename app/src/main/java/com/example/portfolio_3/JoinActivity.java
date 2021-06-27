bf0b18d2d33b4ff763841f0dc2f0322281bae684
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-06-28 08:22:45 +0900

    회원가입

--- app/src/main/java/com/example/portfolio_3/JoinActivity.java
+++ app/src/main/java/com/example/portfolio_3/JoinActivity.java
@@ -7,17 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
-    Button join;
+public class JoinActivity extends AppCompatActivity {
+    Button back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        join = findViewById(R.id.btn_join);
-        join.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_join);
+        back = findViewById(R.id.btn_back);
+        back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, JoinActivity.class));
+                startActivity(new Intent(JoinActivity.this, LoginActivity.class));
             }
         });
     }
--- app/src/main/java/com/example/portfolio_3/MainActivity.java
+++ app/src/main/java/com/example/portfolio_3/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.portfolio_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Handler hand = new Handler();
+
+        hand.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        }, 5000);
+
     }
 }
