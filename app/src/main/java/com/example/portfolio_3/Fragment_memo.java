2c41f39548d2e473d06fc2749cbecda1f7a30c1c
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-07-04 03:04:17 +0900

    로그인기능구현

--- app/src/main/java/com/example/portfolio_3/Fragment_memo.java
+++ app/src/main/java/com/example/portfolio_3/Fragment_memo.java
@@ -4,15 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class Fragment_memo extends Fragment {
-
+    RecyclerView recyclerView;
+    Adapter adapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        
         return inflater.inflate(R.layout.fragment_memo, container, false);
     }
 }
--- app/src/main/java/com/example/portfolio_3/LoginActivity.java
+++ app/src/main/java/com/example/portfolio_3/LoginActivity.java
@@ -17,6 +17,9 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button  mLoginBtn, mJoinBtn;
+    EditText mEmailText, mPasswordText;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +27,46 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
 
+        firebaseAuth =  FirebaseAuth.getInstance();
+        //버튼 등록하기
+        mLoginBtn = findViewById(R.id.btn_login);
+        mEmailText = findViewById(R.id.email);
+        mPasswordText = findViewById(R.id.psw);
+        mJoinBtn = findViewById(R.id.btn_join);
+
+        mJoinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, JoinActivity.class);
+                startActivity(intent);
+            }
+        });
+        //로그인 버튼이 눌리면
+        mLoginBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                String email = mEmailText.getText().toString().trim();
+                String pwd = mPasswordText.getText().toString().trim();
+                if(email.equals("") || pwd.equals("")){
+                    Toast.makeText(LoginActivity.this, "입력하세요.", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                firebaseAuth.signInWithEmailAndPassword(email,pwd)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity2.class);
+                                    startActivity(intent);
+
+                                }else{
+                                    Toast.makeText(LoginActivity.this,"로그인 오류",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+            }}
+        });
     }
 }
