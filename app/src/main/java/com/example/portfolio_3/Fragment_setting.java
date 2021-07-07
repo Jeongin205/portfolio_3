6fadaed51501652c63f83154ef2ef41325007bd4
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-07-07 10:59:15 +0900

    인텐트완료

--- app/src/main/java/com/example/portfolio_3/Fragment_setting.java
+++ app/src/main/java/com/example/portfolio_3/Fragment_setting.java
@@ -1,18 +1,37 @@
 package com.example.portfolio_3;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 public class Fragment_setting extends Fragment {
-
+    Button userset, bgset;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_setting, container, false);
+        userset = v.findViewById(R.id.btn_userset);
+        bgset = v.findViewById(R.id.btn_imgset);
+        userset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), UsersetActivity.class);
+                startActivity(intent);
+            }
+        });
+        bgset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ImgsetActivity.class);
+                startActivity(intent);
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        return v;
     }
 }
