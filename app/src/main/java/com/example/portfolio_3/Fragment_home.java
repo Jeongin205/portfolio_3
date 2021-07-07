4809badd7b57964050652cad61115f33dd7e915e
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-07-07 10:54:52 +0900

    날짜 받아오기

--- app/src/main/java/com/example/portfolio_3/Fragment_home.java
+++ app/src/main/java/com/example/portfolio_3/Fragment_home.java
@@ -1,5 +1,6 @@
 package com.example.portfolio_3;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -13,8 +14,17 @@ public class Fragment_home extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        Button btn_write;
+        btn_write = v.findViewById(R.id.btn_write);
+        btn_write.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), WriteActivity.class);
+                startActivity(intent);
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return v;
     }
 }
--- app/src/main/java/com/example/portfolio_3/WriteActivity.java
+++ app/src/main/java/com/example/portfolio_3/WriteActivity.java
@@ -6,13 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class WriteActivity extends AppCompatActivity {
     Button back;
+    private SimpleDateFormat mFormat = new SimpleDateFormat("yyyy/M/d");
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write);
+        textView = findViewById(R.id.tv_date);
+        Date date = new Date();
+        String time = mFormat.format(date);
+        textView.setText(time);
         back = findViewById(R.id.btn_wback);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
